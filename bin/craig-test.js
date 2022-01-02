43aa618ec44c8e59dec3593ed4b8309da91261b5
Craig <craigmiller160@gmail.com>
2022-01-01 20:14:21 -0500

    Added ability to pass arguments to test command

--- bin/craig-test.js
+++ bin/craig-test.js
@@ -2,6 +2,6 @@
 
 const spawn = require('cross-spawn');
 
-spawn.sync('cross-env', ['NODE_ENV=test', 'jest'], {
+spawn.sync('cross-env', ['NODE_ENV=test', 'jest', ...process.argv.slice(2)], {
     stdio: 'inherit'
 });
