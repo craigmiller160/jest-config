0efb39fb20d477f3b2a2be0e781e28bf4b9a019e
Craig Miller <craigmiller160@gmail.com>
2023-07-24 15:01:09 -0400

    Updating scripts

--- bin/craig-test.js
+++ bin/craig-test.js
@@ -5,5 +5,9 @@ const spawn = require('cross-spawn');
 const result = spawn.sync('cross-env', ['NODE_ENV=test', 'jest', ...process.argv.slice(2)], {
     stdio: 'inherit'
 });
+if (result.error) {
+    console.error(result.error);
+    process.exit(1);
+}
 
 process.exit(result.status);
